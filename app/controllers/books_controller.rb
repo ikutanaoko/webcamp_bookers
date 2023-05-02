ee8e1f08218f30eaca3b71f2a8242ca200a6bbb1
ikutanaoko <132249442+ikutanaoko@users.noreply.github.com>
2023-05-02 16:26:53 +0000

    0503_0126

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,12 +3,16 @@ class BooksController < ApplicationController
     @book = Book.new
     @books =Book.all
   end
-  
+
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
-  end  
+    @books =Book.all
+    @book = Book.new(book_params)
+    if @book.save
+    redirect_to book_path(@book.id)
+    else
+    render :index
+    end
+  end
 
   def show
     @book = Book.find(params[:id])
@@ -17,7 +21,19 @@ class BooksController < ApplicationController
   def edit
     @book = Book.find(params[:id])
   end
-  
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
   private
   def book_params
     params.require(:book).permit(:title, :body)
